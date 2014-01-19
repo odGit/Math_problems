ccf9e796b525dcad0fde46481e9e2856c5f66e6f
Olga Dmitricenko <o.dmitricenko@gmail.com>
2014-01-20 00:34:52 +0100

    changed according to pep 8

--- prob_10_sieve.py
+++ prob_10_sieve.py
@@ -12,23 +12,24 @@ PROBLEM: Find the sum of all the primes below two million.
 import time
 
 def eu_sieve(num):     #sieve of Eratosthenes
+    """ calculate list of primes up to num"""
     sieve = range(num+1)
     
-    for x in xrange(2, int(len(sieve)**0.5) + 1):
+    for n_prime in xrange(2, int(len(sieve)**0.5) + 1):
 
-        if sieve[x] != 0:
-            for i in xrange(x+x, len(sieve), x):
+        if sieve[n_prime] != 0:
+            for i in xrange(n_prime+n_prime, len(sieve), n_prime):
                 sieve[i] = 0
     
-    sieve = [x for x in sieve[2:] if x != 0]
+    sieve = [n_prime for n_prime in sieve[2:] if n_prime != 0]
     
     return sieve
       
         
 start = time.time()
-prime_list = eu_sieve(int(2e6))
-product = sum(prime_list) 
+product = sum(eu_sieve(int(2e6))) 
 elapsed = time.time() - start
 
-print "The sum of all the primes below two million is %s, found in %s sec" %(product, elapsed)
-#The sum of all the primes below two million is 142913828922, found in 0.726412029266 sec
+print "The sum of all the primes < 2 million is %s, found in %s sec" % (product, elapsed)
+#The sum of all the primes below two million is 142913828922,
+# found in 0.726412029266 sec
